8e5688ad145fa41f8d17f57b2a0076c7a4a8779b
Max <falchenkomaxim@gmail.com>
2022-08-30 17:16:17 +0300

    fix /

--- src/main/java/com/example/firsheroku/FirsherokuApplication.java
+++ src/main/java/com/example/firsheroku/FirsherokuApplication.java
@@ -2,12 +2,21 @@ package com.example.firsheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class FirsherokuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FirsherokuApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    ResponseEntity<String> index() {
+        return ResponseEntity.ok("ok FirsherokuApplication");
     }
 
 }
